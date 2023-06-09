3eba2f5030f0a9151ae6260f5cf59a65fe2c2618
ChahrazadBouragba <chbouragba25@gmail.com>
2023-06-09 10:45:22 +0100

    updating data

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import './App.css';
 import Hero from './components/Hero';
 import SectionTwo from './components/sectionTwo';
-import data from './components/data';
+import data from './data';
 
 
 
 
 function App() {
 
-  const artCards = data,map(item => {
-    return ()
+  const artCards = data.map(item => {
+    return (
+      <SectionTwo
+        key = {item.id}
+        {...item}
+      />
+    )
   })
+
   return (
     <div className="artjunks">
       <Hero/>
-      <SectionTwo/>
+      {artCards}
     </div>
   );
 }
